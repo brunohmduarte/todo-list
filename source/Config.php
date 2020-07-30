7aff20239c18bbf77a8991cce293e0c67235b8cf
Bruno Duarte <bruno_ocz86@hotmail.com>
2020-07-30 10:49:49 -0300

    Definindo os valores padrões para as variáveis de configuçães.

--- source/Config.php
+++ source/Config.php
@@ -1,17 +1,18 @@
 <?php
 
+// Define o fuso horário padrão de usado para todas as funções de data/hora da aplicação.
 date_default_timezone_set('America/Sao_Paulo');
 
 // Configuração da hostname padrão da aplicação.
-define("URL_BASE", "http://localhost/teste/to-do-list/");
+define("URL_BASE", "");
 
 // Configuração da conexão com o banco de dados.
 define("DATA_LAYER_CONFIG", [
-    "driver" => "mysql",
-    "host" => "localhost",
-    "port" => "3306",
-    "dbname" => "crud",
-    "username" => "root",
+    "driver" => "",
+    "host" => "",
+    "port" => "",
+    "dbname" => "",
+    "username" => "",
     "passwd" => "",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
